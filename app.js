bfbc99954d0eb58e8086df41db1c79aa475bdba8
Adam Azmanhazim <123760738+1211102573@users.noreply.github.com>
2025-06-26 02:58:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ class App{
 		this.scene.add(ambient);
 		
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
-		this.renderer.setPixelRatio(window.devicePixelRatio * 0.25); // Lower resolution
+		this.renderer.setPixelRatio(window.devicePixelRatio);
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
 		this.renderer.outputEncoding = THREE.sRGBEncoding;
 		
