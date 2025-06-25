e27849ab63dd40df354e97662efcd2b668667d34
Adam Azmanhazim <123760738+1211102573@users.noreply.github.com>
2025-06-26 02:46:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ class App{
         
         const self = this;
         
-        const timeoutId = setTimeout( connectionTimeout, 4000 ); // Increase Delay 
+        const timeoutId = setTimeout(connectionTimeout, 4000); // Increase Delay 
         
         function onSelectStart( event ) {
         
