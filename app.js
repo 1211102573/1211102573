65fb659d30760a13bf8e5e1a035553884c9fb0fd
Adam Azmanhazim <123760738+1211102573@users.noreply.github.com>
2025-06-26 02:05:52 +0800

    Update app.js
    // Directional light added

--- app.js
+++ app.js
@@ -33,6 +33,11 @@ class App{
 		const ambient = new THREE.HemisphereLight(0xFFFFFF, 0xAAAAAA, 0.8);
 		this.scene.add(ambient);
 
+		 const dirLight = new THREE.DirectionalLight(0xffffff, 1); // Directional light added
+  		 dirLight.position.set(5, 10, 7.5);
+   		 dirLight.castShadow = true;
+  		 this.scene.add(dirLight);
+
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
