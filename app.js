e672782c0cb054740afc0cf06128a4632a9c5f8e
Adam Azmanhazim <123760738+1211102573@users.noreply.github.com>
2025-06-26 02:22:16 +0800

    Update app.js
    // Modified Cam FOV

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ class App{
 
 		this.assetsPath = './assets/';
         
-		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.01, 500 );
+		this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.01, 500 );
 		this.camera.position.set( 0, 1.6, 0 );
         
         this.dolly = new THREE.Object3D(  );
